bd50138e0afbee0cbfa53e5ad50d6fab53e413cf
Sergey Patoka <maior1998@mail.ru>
2019-02-10 09:48:49 +0500

    Изменено пространство имен Matrix.

--- KMZILib/Maths/Matrix.cs
+++ KMZILib/Maths/Matrix.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KMZILib.Maths
+namespace KMZILib
 {
     /// <summary>
     /// Представляет собой матрицу. Необязательно прямоугольную.
--- Ручной тест/Program.cs
+++ Ручной тест/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using KMZILib;
-using KMZILib.Maths;
 using Vector=KMZILib.Vector;
 
 namespace Ручной_тест
