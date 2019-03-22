dbb12a124fc6b761d0b8dde6c15558e5a677f8ad
Sergey Patoka <maior1998@mail.ru>
2019-03-22 16:27:02 +0500

    Гиперболический синус уже есть во встроенной математике.

--- KMZILib/Misc/Misc.cs
+++ KMZILib/Misc/Misc.cs
@@ -4,18 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KMZILib.Misc
+namespace KMZILib
 {
     /// <summary>
     /// Разная всячина. Полезные функции и методы.
     /// </summary>
     public static partial class Misc
     {
-        /// <summary>
-        /// Гиперболический синус.
-        /// </summary>
-        /// <param name="x"></param>
-        /// <returns></returns>
-        public static double sh(double x) => (Math.Exp(x)-Math.Exp(-x))/2;
+        
     }
 }
