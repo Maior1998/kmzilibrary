28e04619bd0b21a7cce0e46a470efe2383a1ac95
Sergey Patoka <maior1998@mail.ru>
2019-03-22 16:20:48 +0500

    Начало класса с дополнительными полезными функциями. Гиперболичекий синус.

---
+++ KMZILib/Misc/Misc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMZILib.Misc
+{
+    /// <summary>
+    /// Разная всячина. Полезные функции и методы.
+    /// </summary>
+    public static partial class Misc
+    {
+        /// <summary>
+        /// Гиперболический синус.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static double sh(double x) => (Math.Exp(x)-Math.Exp(-x))/2;
+    }
+}
